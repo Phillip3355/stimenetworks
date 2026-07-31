3c917388b6d8de1ce1751766c4a403a9fb71e346
Phillip <kimjc.120211@gmail.com>
2026-07-31 21:36:04 +0900

    voice patch1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/voice-:roomCode',
+        destination: '/voice/:roomCode',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
